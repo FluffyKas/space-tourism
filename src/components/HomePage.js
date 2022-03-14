2ef997ca7f3fa6d6c707d157acb530bed74b805e
FluffyKas <maruzs.zso@gmail.com>
2022-03-14 14:36:07 +0100

    Adjusting styles for tablet and desktop view, adding wrappers to markup

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,10 +2,14 @@ import './HomePage.scss';
 
 const HomePage = () => {
   return (
-    <div className="home">
-      <h1 className="home__title">So, you want to travel to <span className="home__title-highlight">Space</span></h1>
-      <p className="home__text">Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
-      <button className="explore-btn">Explore</button>
+    <div className="home-outer-wrapper">
+      <div className="home">
+        <div className="home-text-wrapper">
+          <h1 className="home__title">So, you want to travel to <span className="home__title-highlight">Space</span></h1>
+          <p className="home__text">Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+        </div>
+        <button className="explore-btn">Explore</button>
+      </div>
     </div>
   );
 }
