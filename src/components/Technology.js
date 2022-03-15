7720f818d0acdae8cdbdcf7d9931f1d691e3ceb0
FluffyKas <maruzs.zso@gmail.com>
2022-03-15 13:03:12 +0100

    Adding basic markup

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -1,6 +1,27 @@
 const Technology = () => {
+
+  const data = require('../assets/data.json');
+  const techData = data.technology;
+
+
   return (
-    <p>Tech!</p>
+    <div>
+
+      <h1>03 Space launch 101</h1>
+
+      <picture>
+        <img src="" alt="" />
+      </picture>
+
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+
+      <h2>The terminology...</h2>
+      <h3>{techData.name}</h3>
+      <p>{techData.description}</p>
+
+    </div>
   );
 }
 
