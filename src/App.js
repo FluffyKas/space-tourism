c19e6a797aeac81d577a767d4116387a3778878f
FluffyKas <maruzs.zso@gmail.com>
2022-03-15 11:45:17 +0100

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
+import Destination from './components/Destination';
+import Crew from './components/Crew';
+import Technology from './components/Technology';
 
 
 function App() {
@@ -7,7 +11,12 @@ function App() {
     <>
       <Header />
       <main>
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
       </main>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './global-styles/reset.scss';
 import './global-styles/variables.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
