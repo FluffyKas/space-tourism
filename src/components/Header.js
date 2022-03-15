cb4514e0336860d2a9760ea6c13ff4f5fc1b66f2
FluffyKas <maruzs.zso@gmail.com>
2022-03-15 11:46:07 +0100

    Adding router links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import logo from '../assets/shared/logo.svg';
 import hamburger from '../assets/shared/icon-hamburger.svg';
 import './Header.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -12,10 +13,10 @@ const Header = () => {
             <img src={hamburger} alt="" />
           </button>
           <ul className="nav-menu">
-            <li className="menu-item"><span className="menu-item-number">00</span>Home</li>
-            <li className="menu-item"><span className="menu-item-number">01</span>Destination</li>
-            <li className="menu-item"><span className="menu-item-number">02</span>Crew</li>
-            <li className="menu-item"><span className="menu-item-number">03</span>Technology</li>
+            <Link to="/" className="menu-item"><span className="menu-item-number">00</span>Home</Link>
+            <Link to="/destination" className="menu-item"><span className="menu-item-number">01</span>Destination</Link>
+            <Link to="/crew" className="menu-item"><span className="menu-item-number">02</span>Crew</Link>
+            <Link to="/technology" className="menu-item"><span className="menu-item-number">03</span>Technology</Link>
           </ul>
         </nav>
       </div>
