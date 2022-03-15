c60ed43eb96ada98d93ca526b8d3ac82f4764ad8
FluffyKas <maruzs.zso@gmail.com>
2022-03-15 17:00:08 +0100

    Fixing image, adding mobile view styles

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -15,6 +15,7 @@ const Technology = () => {
       <div className="inner-container">
         <h1 className="page-title"><span className="page-title-number">03</span> Space launch 101</h1>
 
+        <img src={techData[0].images.landscape} alt="" className="technology-illustration" />
 
         <li className="btn-container">
           {techData.map((techOption, index) => {
@@ -24,8 +25,8 @@ const Technology = () => {
         </li>
 
         <h2 className="const-subtitle">The terminology...</h2>
-        <h3 className="dynamic-subtitle">{techData[1].name}</h3>
-        <p className="page-description">{techData[1].description}</p>
+        <h3 className="dynamic-subtitle">{techData[0].name}</h3>
+        <p className="page-description">{techData[0].description}</p>
       </div>
 
     </div>
