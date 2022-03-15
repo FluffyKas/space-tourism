6b1bac6dd870c83a54bb33a13ffd8403647780f9
FluffyKas <maruzs.zso@gmail.com>
2022-03-15 17:24:06 +0100

    Adding containers and some desktop styles

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -12,21 +12,32 @@ const Technology = () => {
   return (
     <div className="technology-container">
 
+
+      <h1 className="page-title"><span className="page-title-number">03</span> Space launch 101</h1>
+
       <div className="inner-container">
-        <h1 className="page-title"><span className="page-title-number">03</span> Space launch 101</h1>
 
-        <img src={techData[0].images.landscape} alt="" className="technology-illustration" />
+        <picture>
+          <source media="(min-width:30rem)" srcSet={techData[0].images.portrait} />
+          <img src={techData[0].images.landscape} alt="" className="technology-illustration" />
+        </picture>
 
-        <li className="btn-container">
-          {techData.map((techOption, index) => {
-            return <button key={index} className="tech-page-btn" onClick={changeContent}>{index + 1}</button>
-          })
-          }
-        </li>
+        <div className="tech-text">
+          <li className="btn-container">
+            {techData.map((techOption, index) => {
+              return <button key={index} className="tech-page-btn" onClick={changeContent}>{index + 1}</button>
+            })
+            }
+          </li>
 
-        <h2 className="const-subtitle">The terminology...</h2>
-        <h3 className="dynamic-subtitle">{techData[0].name}</h3>
-        <p className="page-description">{techData[0].description}</p>
+          <div>
+            <h2 className="const-subtitle">The terminology...</h2>
+            <h3 className="dynamic-subtitle">{techData[0].name}</h3>
+            <p className="page-description">{techData[0].description}</p>
+          </div>
+
+        </div>
+
       </div>
 
     </div>
