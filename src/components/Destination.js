e906b7932405dae5c8d0bf2cf2c33076f872b9d4
FluffyKas <maruzs.zso@gmail.com>
2022-03-16 13:56:21 +0100

    Adding basic markup, changing bg image urls

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,10 +1,40 @@
+import json from '../assets/data.json';
 import './Destination.scss';
 
 const Destination = () => {
+
+  const destinationData = json.destinations;
+
   return (
     <div className="destination-container">
       <div className="inner-container">
-        cica
+
+        <h1 className="page-title"><span className="page-title-number">01</span> Pick your destination</h1>
+
+        <picture>
+          <source media="(min-width:30rem)" srcSet={destinationData[0].images.png} />
+          <img src={destinationData[0].images.webp} alt="" className="destination-illustration" />
+        </picture>
+
+        <div className="destination-text">
+          <li className="btn-container">
+            {destinationData.map((destinationOption, index) => {
+              return <button key={index} className="destination-page-btn">{destinationOption.name}</button>
+            })
+            }
+          </li>
+
+          <div>
+            <h2 className="dynamic-subtitle">{destinationData[0].name}</h2>
+            <p className="page-description">{destinationData[0].description}</p>
+
+            <h3>Avg. distance</h3>
+            <p>{destinationData[0].distance}</p>
+            <h3>Est. travel time</h3>
+            <p>{destinationData[0].travel}</p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
