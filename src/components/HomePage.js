1ea1f133f3e7263442264e7f188f1773e4b8d879
FluffyKas <maruzs.zso@gmail.com>
2022-03-17 14:12:32 +0100

    Organising code

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,17 +6,20 @@ const HomePage = () => {
   return (
     <div className="home-outer-wrapper">
       <div className="home">
+
         <div className="home-text-wrapper">
           <h1 className="home__title uppercase pastel-blue ff-barlow-cond">So, you want to travel to <span className="home__title-highlight white ff-bellefair">Space</span></h1>
           <p className="home__text pastel-blue ff-barlow">Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
         </div>
+
         <motion.button
           className="explore-btn ff-bellefair uppercase"
           whileHover={{ boxShadow: "0 0 0 40px hsla(0, 0%, 100%, 0.175)" }}
-          exit={{ boxShadow: "0" }}
           transition={{ ease: "easeOut", duration: 0.35 }}
         >
-          Explore</motion.button>
+          Explore
+        </motion.button>
+
       </div>
     </div>
   );
