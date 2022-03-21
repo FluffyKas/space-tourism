1f42c1195de474527e923d37e8dae9545fb3d0f3
FluffyKas <maruzs.zso@gmail.com>
2022-03-21 12:31:41 +0100

    Adding classes and basic styles

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -1,32 +1,37 @@
 import json from '../assets/data.json';
 import './Crew.scss';
+import '../global-styles/utilities.scss';
 
 const Crew = () => {
 
   const crewData = json.crew;
 
   return (
-    <div className="crew-container">
+    <div className="crew-container container white">
+
+      <h1 className="page-title"><span className="page-title-number">02</span> Meet your crew</h1>
+
       <div className="inner-container">
 
-        <h1 className="page-title"><span className="page-title-number">02</span> Meet your crew</h1>
-
-        <picture>
+        <picture className="crew-img">
           <source media="(min-width:30rem)" srcSet={crewData[0].images.png} />
           <img src={crewData[0].images.webp} alt="" className="crew-illustration" />
         </picture>
 
         <div className="destination-text">
-          <li className="btn-container">
+
+          <li className="crew-btn-container">
             {crewData.map((crewOption, index) => {
-              return <button key={index} className="destination-page-btn">.</button>
+              return <button key={index} className="crew-page-btn">
+                <div className="btn-circle"></div>
+              </button>
             })
             }
           </li>
 
-          <div>
-            <h2 className="const-subtitle">{crewData[0].role}</h2>
-            <h3 className="dynamic-subtitle">{crewData[0].name}</h3>
+          <div className="crew-text">
+            <h2 className="job-title ff-bellefair uppercase fw-400 pb-050">{crewData[0].role}</h2>
+            <h3 className="name ff-bellefair uppercase fw-400 pb-100">{crewData[0].name}</h3>
             <p className="page-description">{crewData[0].bio}</p>
           </div>
 
