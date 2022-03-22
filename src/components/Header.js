a0f5176074b2d6bc605e765e54efafa31d79c8f5
FluffyKas <maruzs.zso@gmail.com>
2022-03-22 16:40:14 +0100

    Adding skip nav button, focus styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,32 @@ import logo from '../assets/shared/logo.svg';
 import hamburger from '../assets/shared/icon-hamburger.svg';
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+
+  const [activeHeader, setActiveHeader] = useState('');
+
+  const toggleActiveState = (e) => {
+    setActiveHeader(e.target.textContent)
+  }
+
   return (
     <header>
+      <button className="nav-skip">Skip navigation</button>
       <div className="header-wrapper">
-        <Link to="/">
-          <img src={logo} alt="Space Tourism." className="logo" />
+        <Link to="/" className="logo">
+          <img src={logo} alt="Space Tourism." />
         </Link>
         <nav>
           <button className="hamburger-btn">
             <img src={hamburger} alt="" />
           </button>
           <ul className="nav-menu">
-            <Link to="/" className="menu-item"><span className="menu-item-number">00</span>Home</Link>
-            <Link to="/destination" className="menu-item"><span className="menu-item-number">01</span>Destination</Link>
-            <Link to="/crew" className="menu-item"><span className="menu-item-number">02</span>Crew</Link>
-            <Link to="/technology" className="menu-item"><span className="menu-item-number">03</span>Technology</Link>
+            <Link to="/" className="menu-item" onClick={toggleActiveState}><span className="menu-item-number">00</span>Home</Link>
+            <Link to="/destination" className="menu-item" onClick={toggleActiveState}><span className="menu-item-number">01</span>Destination</Link>
+            <Link to="/crew" className="menu-item" onClick={toggleActiveState}><span className="menu-item-number">02</span>Crew</Link>
+            <Link to="/technology" className="menu-item" onClick={toggleActiveState}><span className="menu-item-number">03</span>Technology</Link>
           </ul>
         </nav>
       </div>
