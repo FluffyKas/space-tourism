6e0663b0b405873e8b80b1c031e0c07e8c28b043
FluffyKas <maruzs.zso@gmail.com>
2022-03-22 14:44:36 +0100

    Moving JSON data to top level

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import HomePage from './components/HomePage';
 import Destination from './components/Destination';
 import Crew from './components/Crew';
 import Technology from './components/Technology';
-
+import json from './assets/data.json';
 
 function App() {
+
   return (
     <>
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
+          <Route path="/destination" element={<Destination data={json.destinations} />} />
+          <Route path="/crew" element={<Crew data={json.crew} />} />
+          <Route path="/technology" element={<Technology data={json.technology} />} />
         </Routes>
       </main>
     </>
