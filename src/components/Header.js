10ffa8bf982b172d52aeea61805793c1c4f37570
FluffyKas <maruzs.zso@gmail.com>
2022-03-24 14:58:13 +0100

    Final styles applied

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import logo from '../assets/shared/logo.svg';
 import hamburger from '../assets/shared/icon-hamburger.svg';
+import closeBtn from '../assets/shared/icon-close.svg';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
@@ -35,29 +36,31 @@ const Header = () => {
   return (
     <header className="ff-barlow-cond white">
       <Link to="#main" className="nav-skip uppercase">Skip navigation</Link>
-      <div className="header-wrapper">
-        <Link to="/" className="logo">
-          <img src={logo} alt="Space Tourism." />
-        </Link>
+      <Link to="/" className="logo">
+        <img src={logo} alt="Space Tourism." />
+      </Link>
 
-        <nav>
+      <nav>
 
-          <button className="hamburger-btn" aria-expanded={isOpened ? "true" : "false"} aria-controls="nav-menu" onClick={toggleMenu}>
-            <img src={hamburger} alt="" />
-            <span className="sr-only">Menu</span>
-          </button>
+        <button className="hamburger-btn" aria-expanded={isOpened ? "true" : "false"} aria-controls="nav-menu" onClick={toggleMenu}>
+          {
+            isOpened ?
+              <img src={closeBtn} alt="" aria-hidden="true" /> :
+              <img src={hamburger} alt="" aria-hidden="true" />
+          }
+          <span className="sr-only">Menu</span>
+        </button>
 
-          {(isOpened || screenWidth > 640) && (
-            <ul id="nav-menu" className="nav-menu uppercase">
-              <li><Link to="/" className="menu-item"><span className="menu-item-number" aria-hidden="true">00</span>Home</Link></li>
-              <li><Link to="/destination" className="menu-item"><span className="menu-item-number" aria-hidden="true">01</span>Destination</Link></li>
-              <li><Link to="/crew" className="menu-item"><span className="menu-item-number" aria-hidden="true">02</span>Crew</Link></li>
-              <li><Link to="/technology" className="menu-item"><span className="menu-item-number" aria-hidden="true">03</span>Technology</Link></li>
-            </ul>
-          )}
-        </nav>
+        {(isOpened || screenWidth > 640) && (
+          <ul id="nav-menu" className="nav-menu uppercase">
+            <li><Link to="/" className="menu-item"><span className="menu-item-number" aria-hidden="true">00</span>Home</Link></li>
+            <li><Link to="/destination" className="menu-item"><span className="menu-item-number" aria-hidden="true">01</span>Destination</Link></li>
+            <li><Link to="/crew" className="menu-item"><span className="menu-item-number" aria-hidden="true">02</span>Crew</Link></li>
+            <li><Link to="/technology" className="menu-item"><span className="menu-item-number" aria-hidden="true">03</span>Technology</Link></li>
+          </ul>
+        )}
+      </nav>
 
-      </div>
     </header >
   );
 }
