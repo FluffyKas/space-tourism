f3e6938d872536623bb6a764f224d7a4ff8494b2
FluffyKas <maruzs.zso@gmail.com>
2022-05-03 17:06:54 +0200

    Changed imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import './global-styles/utilities.scss'
+import './assets/global-styles/utilities.scss';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components//header/Header';
-import HomePage from './components/homepage/HomePage';
-import Destination from './components//destination/Destination';
-import Crew from './components/crew/Crew';
-import Technology from './components/technology/Technology';
+import { HomePage } from './pages';
+import { Destination } from './pages';
+import { Crew } from './pages';
+import { Technology } from './pages';
 import json from './assets/data.json';
+import { AnimatePresence } from "framer-motion";
 
 function App() {
 
   return (
     <>
       <Header />
-      <main id="main">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/destination" element={<Destination data={json.destinations} />} />
-          <Route path="/crew" element={<Crew data={json.crew} />} />
-          <Route path="/technology" element={<Technology data={json.technology} />} />
-        </Routes>
-      </main>
+      <AnimatePresence>
+        <main id="main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/destination" element={<Destination data={json.destinations} />} />
+            <Route path="/crew" element={<Crew data={json.crew} />} />
+            <Route path="/technology" element={<Technology data={json.technology} />} />
+          </Routes>
+        </main>
+      </AnimatePresence>
     </>
   );
 }
