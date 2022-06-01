e085d426ac54fe5321c24f73d1925274cb7534e0
FluffyKas <maruzs.zso@gmail.com>
2022-06-01 16:36:00 +0200

    animations: add animations to text & image

--- src/pages/crew/Crew.js
+++ src/pages/crew/Crew.js
@@ -2,7 +2,7 @@ import './Crew.scss';
 import { useState } from 'react';
 import { ButtonContainer } from '../../components';
 import { AnimatePresence, motion } from 'framer-motion';
-import { pageAnimation, textContainerVariants, imgVariants, placeHolderVariants } from '../../assets/animations/animations';
+import { pageAnimation } from '../../assets/animations/animations';
 
 export const Crew = ({ data }) => {
 
@@ -21,36 +21,39 @@ export const Crew = ({ data }) => {
       <div className="inner-container crew-container-inner">
 
 
-        <AnimatePresence exitBeforeEnter>
-          <motion.div className="crew-img"
-            key={activeTab}
-            variants={placeHolderVariants}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-          >
-            <motion.picture variants={imgVariants} initial="hidden" animate="visible" key={activeTab}>
+        <motion.div className="crew-img"
+        >
+          <AnimatePresence exitBeforeEnter>
+            <motion.picture
+              key={activeTab}
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+              exit={{ opacity: 0, x: 50, transition: { duration: 1 } }}
+            >
               <source media="(min-width:30rem)" srcSet={data[activeTab].images.png} />
               <img src={data[activeTab].images.webp} alt={data[activeTab].name} />
             </motion.picture>
+          </AnimatePresence>
+        </motion.div>
+
+        <AnimatePresence exitBeforeEnter>
+          <motion.div className="crew-text"
+            key={activeTab}
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+            exit={{ opacity: 0, x: -50, transition: { duration: 1 } }}
+          >
+
+            <ButtonContainer name={'crew'} data={data} activeTab={activeTab} setActiveTab={setActiveTab} />
+
+            <div className="crew-text">
+              <h2 className="job-title ff-bellefair uppercase fw-400 pb-050">{data[activeTab].role}</h2>
+              <h3 className="name ff-bellefair uppercase fw-400 pb-100">{data[activeTab].name}</h3>
+              <p className="page-description">{data[activeTab].bio}</p>
+            </div>
+
           </motion.div>
         </AnimatePresence>
-
-        <motion.div className="crew-text"
-          variants={textContainerVariants}
-          initial="hidden"
-          animate="visible"
-        >
-
-          <ButtonContainer name={'crew'} data={data} activeTab={activeTab} setActiveTab={setActiveTab} />
-
-          <div className="crew-text">
-            <h2 className="job-title ff-bellefair uppercase fw-400 pb-050">{data[activeTab].role}</h2>
-            <h3 className="name ff-bellefair uppercase fw-400 pb-100">{data[activeTab].name}</h3>
-            <p className="page-description">{data[activeTab].bio}</p>
-          </div>
-
-        </motion.div>
       </div>
     </motion.div >
   );
